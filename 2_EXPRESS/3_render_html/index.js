9e618281a9e016eb457f1613e3299ee7fc7603f2
Gabriella Victória <gabriella.victoria@eaportal.org>
2023-10-05 07:56:21 -0300

    render html com express

--- 2_EXPRESS/3_render_html/index.js
+++ 2_EXPRESS/3_render_html/index.js
@@ -1,12 +1,20 @@
 const express = require("express")
+const path = require("path")
 
 const app = express()
 
 
+const caminho = path.join(__dirname, 'templates')
+
+
+
 app.get('/', (requisicao, resposta) => {
-    resposta.send("Seja bem vindo ao servidor ")
+    resposta.sendFile(`${caminho}/index.html`)
 })
 
 app.listen(3000, () => {
     console.log("Servidor rodando na porta 3000")
 })
+
+
+
